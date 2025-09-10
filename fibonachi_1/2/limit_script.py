762efa5976dbdae8eb0f0b4819d07b65807c404e
Oleksii Dashchuk <oleksii.dashchuk@lnu.edu.ua>
2025-09-10 22:11:57 +0300

    Modify Fibonacci script to handle start and end limits

--- fibonachi_1/2/limit_script.py
+++ fibonachi_1/2/limit_script.py
@@ -1,25 +1,29 @@
 with open("limit_file.txt", "r") as file:
    line_one=file.readline()
    steps=file.readline()
-   limit=file.readline()
-array=[]
+   limit_start = file.readline()
+   limit_end=file.readline()
 line_one=line_one.split()
-for i in line_one:
-    array.append(i)
-    array=[float(i) for i in array]
-for i in steps and limit:
-    steps=float(steps)
-    limit=float(limit)
-def fibonaci(steps, array, limit):
-    if steps==0:
-        return array
-    if(limit>= array[-1]):
-     array.append(array[-1]+ array[-2])
-     steps-=1
-    elif(limit<array[-1]):
-        array.pop(-1)
-        return array
-    return fibonaci(steps, array,limit)
+
+array=[float(i) for i in line_one]
+steps=float(steps)
+limit_end=float(limit_end)
+limit_start=float(limit_start)
+def fibonaci(steps, array, limit_end,limit_start):
+    while steps > 0:
+        next_number=array[-1]+array[-2]
+        if next_number > limit_end:
+            break
+        array.append(next_number)
+        steps-=1
+
+    filtered = []
+    for num in array:
+        if num > limit_start:
+            filtered.append(num)
+    return filtered
+
 with open("limit_result.txt", "w") as file:
-    file.write(str(fibonaci(steps, array,limit)))
-    file.write("\n")
+    result = fibonaci(steps, array, limit_end, limit_start)
+    file.write(str(result) + "\n")
+
